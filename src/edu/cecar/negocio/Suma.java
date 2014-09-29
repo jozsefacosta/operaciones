204f072953de9aacbad26ad3ebd7b5d1b044ee24
jozsefacosta <jozsef3@hotmail.com>
2014-09-29 14:30:10 -0500

    Commit 1. (Suma)

---
+++ src/edu/cecar/negocio/Suma.java
@@ -0,0 +1,17 @@
+package edu.cecar.negocio;
+
+public class Suma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=2;
+		int b=4;
+		
+		int suma = a+b;
+		
+		System.out.println("La suma es: "+suma);
+
+	}
+
+}
