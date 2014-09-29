dd45efb2b2fe888c500f7ba5e54007fde6e22359
jozsefacosta <jozsef3@hotmail.com>
2014-09-29 15:30:50 -0500

    Commit. (Agregue clase Resta)

---
+++ src/edu/cecar/negocio/Resta.java
@@ -0,0 +1,17 @@
+package edu.cecar.negocio;
+
+public class Resta {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=2;
+		int b=4;
+		
+		int resta = a-b;
+		
+		System.out.println("La resta es: "+resta);
+
+	}
+
+}
